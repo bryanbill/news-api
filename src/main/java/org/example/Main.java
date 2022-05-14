3ef0f66d01cd802d07f0667e1f7baa7abdfecab4
bryanbill <emobrya@gmail.com>
2022-05-14 12:10:19 +0300

    ft:Routes - Implements all api routes and methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.database.DbImpl;
+import org.example.database.Seeder;
+import org.example.utils.Router;
+import org.sql2o.Connection;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection connection = new DbImpl().connect();
+        Seeder.seed(connection);
+        new Router().run(connection);
     }
 }
